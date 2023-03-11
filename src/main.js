63b8e77e0bfba370c3790d7bd70a360ca8a57667
Lucas Monteiro <lucassmonn@gmail.com>
2023-03-11 18:23:41 -0300

    feat: add node-cron

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import * as cron from 'node-cron';
 import { getSummary } from './openai.js';
 import { getMovieData } from './themoviesdb.js';
 import { postTweet, replyTweet } from './twitter.js';
@@ -6,16 +7,16 @@ import { createMedia, makeReply } from './utils.js';
 
 dotenv.config();
 
-(async () => {
+console.log('Starting...')
+cron.schedule(process.env.CRON, (async () => {
     try {
-        console.log('Starting...')
         const movie = await getMovieData();
         await createMedia(`${process.env.THE_MOVIE_CDN}/${movie.poster_path}`)
         const summary = await getSummary(movie.overview);
         const tweet = await postTweet(summary);
         await replyTweet(makeReply(movie), tweet.data.id)
-        console.log('Done!')
+        console.log(`Tweeted successfully - ${movie.title}`)
     } catch (e) {
         console.log(e)
     }
-})();
+}))
